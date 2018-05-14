413d9887c1f66e9cca2e5f273a4fb3efc9356589
Roget Neo Alabastro <rocketneo@gmail.com>
2018-05-13 17:42:42 -1000

    Made things fancy with some colors :)

--- drivebackup.py
+++ drivebackup.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import shutil
 import distutils
 from distutils import dir_util
@@ -9,6 +9,10 @@ dest = "/Volumes/Googledrive/My Drive/Backup/Audio/LogicProjects/"
 list = os.listdir(path)
 
 itemAmount = 0
+
+CGreen = "\033[38:5:10m"
+CRed = "\033[38:5:9m"
+CEnd = "\033[0m"
 
 if len(os.listdir(dest)) == 0: # Checks if the directory is empty. If it is, copies files into it
     print("Destination directory empty, copying files.")
@@ -23,28 +27,35 @@ if len(os.listdir(dest)) == 0: # Checks if the directory is empty. If it is, cop
         
     print("backup complete. \rcopied " + str(itemAmount) + " items")
 else: # When directory is not empty, overwrite and backup. 
-    for i in range(len(list)):
-        
-        statusSource = src+list[i]+"/Resources/ProjectInformation.plist"
-        statusSourceVol = dest+list[i]+"/Resources/ProjectInformation.plist"
+    try:
+        for i in range(len(list)):
             
-        if not list[i].startswith('.'):
-            for j in range(len(list)):
-                try:
-                    if str(list[i]) == str(list[j]) and format(os.stat(statusSource).st_mtime) != format(os.stat(statusSourceVol).st_mtime) and not list[i].startswith('.'):
+            statusSource = src+list[i]+"/Resources/ProjectInformation.plist"
+            statusSourceVol = dest+list[i]+"/Resources/ProjectInformation.plist"
+                
+            if not list[i].startswith('.'):
+                for j in range(len(list)):
+                    try:
+                        if str(list[i]) == str(list[j]) and format(os.stat(statusSource).st_mtime) != format(os.stat(statusSourceVol).st_mtime) and not list[i].startswith('.'):
+                            itemAmount += 1
+                            print("copying: " + list[i])
+                            try:
+                                distutils.dir_util.copy_tree(src+list[i], dest)#+list[j]
+                            except FileNotFoundError:
+                                print(CRed + src+list[i] + " not found, skipping"+ CEnd)
+                                pass
+                    except FileNotFoundError:
                         itemAmount += 1
                         print("copying: " + list[i])
-                        try:
-                            distutils.dir_util.copy_tree(src+list[i], dest)#+list[j]
-                        except FileNotFoundError:
-                            print(src+list[i] + " not found, skipping")
-                            pass
-                except FileNotFoundError:
-                    itemAmount += 1
-                    print("copying: " + list[i])
-                    distutils.dir_util.copy_tree(src+list[i], dest)
-                
+                        distutils.dir_util.copy_tree(src+list[i], dest)
+
+    except KeyboardInterrupt:
+            print(CRed + " Failed" + CEnd)
+            try:
+                sys.exit(0)
+            except SystemExit:
+                os._exit(0)
                     
 
-print("backup complete.")
+print(CGreen + "backup complete." + CEnd)
 print("saved " + str(itemAmount) + " out of " + str(len(list)) + " items")
